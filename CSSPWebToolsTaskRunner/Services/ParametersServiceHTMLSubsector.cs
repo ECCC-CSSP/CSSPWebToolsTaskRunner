124ff6c108a4108c02e7be6fb74d516af255030d
charlesnathalie <charlesleblanc66@gmail.com>
2022-04-27 09:29:17 -0300

    Update ParametersServiceHTMLSubsector.cs

--- CSSPWebToolsTaskRunner/Services/ParametersServiceHTMLSubsector.cs
+++ CSSPWebToolsTaskRunner/Services/ParametersServiceHTMLSubsector.cs
@@ -49,6 +49,20 @@ namespace CSSPWebToolsTaskRunner.Services
                         }
                     }
                     break;
+                case "WQMSitesPhotoAlbumENDOCX":
+                case "WQMSitesPhotoAlbumFRDOCX":
+                    {
+                        if (!GenerateHTMLWQMSitesPhotoAlbumDocx())
+                        {
+                            return false;
+                        }
+
+                        if (!GenerateObjects())
+                        {
+                            return false;
+                        }
+                    }
+                    break;
                 case "FCSummaryStatFRXLSX":
                 case "FCSummaryStatENXLSX":
                     {
@@ -235,7 +249,7 @@ namespace CSSPWebToolsTaskRunner.Services
                         {
                             type = "DailyDuplicate";
                         }
-                        else if(mwqmSample.SampleTypesText.Contains($"{ ((int)SampleTypeEnum.Infrastructure).ToString() },"))
+                        else if (mwqmSample.SampleTypesText.Contains($"{ ((int)SampleTypeEnum.Infrastructure).ToString() },"))
                         {
                             type = "Infrastructure";
                         }
@@ -295,6 +309,241 @@ namespace CSSPWebToolsTaskRunner.Services
                 sb.AppendLine("</html>");
 
                 FileInfo fi = new FileInfo($@"C:\CSSP\{ locator } AllWQData.html");
+
+                StreamWriter sw = fi.CreateText();
+                sw.WriteLine(sb.ToString());
+                sw.Close();
+
+                return true;
+            }
+        }
+
+        private bool GenerateHTMLWQMSitesPhotoAlbumDocx()
+        {
+            string NotUsed = "";
+
+            int SubsectorTVItemID = 0;
+
+            List<string> ParamValueList = Parameters.Split("|||".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string tempVal = GetParameters("TVItemID", ParamValueList);
+            if (string.IsNullOrWhiteSpace(tempVal))
+            {
+                NotUsed = string.Format(TaskRunnerServiceRes._IsRequired, "TVItemID");
+                List<TextLanguage> TextLanguageList = _TaskRunnerBaseService.GetTextLanguageFormat1List("_IsRequired", "TVItemID");
+            }
+            SubsectorTVItemID = int.Parse(tempVal);
+
+            using (CSSPDBEntities db2 = new CSSPDBEntities())
+            {
+                var subsectorItem = (from c in db2.TVItems
+                                     from cl in db2.TVItemLanguages
+                                     where c.TVItemID == cl.TVItemID
+                                     && c.TVItemID == SubsectorTVItemID
+                                     && cl.Language == (int)LanguageEnum.en
+                                     select new { c, cl }).FirstOrDefault();
+
+
+                string tvText = subsectorItem.cl.TVText;
+                string locator = tvText;
+                string name = tvText;
+
+                if (tvText.Contains(" "))
+                {
+                    locator = tvText.Substring(0, tvText.IndexOf(" "));
+                    name = tvText.Substring(tvText.IndexOf(" ") + 1);
+                }
+
+                name = name.Trim();
+
+                if (!string.IsNullOrWhiteSpace(name) && name.StartsWith("("))
+                {
+                    name = name.Substring(1);
+                }
+                if (!string.IsNullOrWhiteSpace(name) && name.StartsWith("("))
+                {
+                    name = name.Substring(1);
+                }
+
+                if (!string.IsNullOrWhiteSpace(name) && name.EndsWith(")"))
+                {
+                    name = name.Substring(0, name.Length - 1);
+                }
+                if (!string.IsNullOrWhiteSpace(name) && name.EndsWith(")"))
+                {
+                    name = name.Substring(0, name.Length - 1);
+                }
+
+                NotUsed = string.Format(TaskRunnerServiceRes.Creating_, $"{ locator } SitePhotoAlbum.html");
+                List<TextLanguage> TextLanguageList = _TaskRunnerBaseService.GetTextLanguageFormat1List("Creating_", $"{ locator } SitePhotoAlbum.html");
+
+                _TaskRunnerBaseService.SendStatusTextToDB(TextLanguageList);
+
+                _TaskRunnerBaseService.SendPercentToDB(_TaskRunnerBaseService._BWObj.appTaskModel.AppTaskID, 10);
+
+
+                /*
+                <!DOCTYPE html>
+                <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+                <head>
+                    <meta charset="utf-8" />
+                    <title>NB-06-02-002 Site Photo Album</title>
+                </head>
+
+                <body>
+                    <h2>NB-06-02-002 Bouctouche (Bouctouche River and Harbour)</h2>
+                    <table style="width: '95%'; height: '95%';">
+                        <tr style="height: 32%">
+                            <td style="width: '45%'; border: 1px solid red">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                            <td style="width: '45%'; border: 1px solid blue">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                        </tr>
+                        <tr style="height: 32%">
+                            <td style="width: '45%'; border: 1px solid red">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                            <td style="width: '45%'; border: 1px solid blue">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                        </tr>
+                        <tr style="height: 32%">
+                            <td style="width: '45%'; border: 1px solid red">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                            <td style="width: '45%'; border: 1px solid blue">
+                                <img width="45%" height="32%"
+                                    src="http://localhost:11562/en-CA/File/FileDownload?TVFileTVItemID=331295">
+                                <br>
+                                <span>testingljs eflijs feli </span>
+                            </td>
+                        </tr>
+                    </table>
+                </body>
+
+                </html> 
+                 */
+
+
+                sb.AppendLine("<!DOCTYPE html>");
+                sb.AppendLine("");
+                sb.AppendLine(@"<html lang=""en"" xmlns=""http://www.w3.org/1999/xhtml"">");
+                sb.AppendLine("<head>");
+                sb.AppendLine(@"    <meta charset=""utf-8"" />");
+                sb.AppendLine($"    <title>{ tvText }</title>");
+                sb.AppendLine("</head>");
+                sb.AppendLine("<body>");
+
+                var tvItemList = (from c in db2.TVItems
+                                  from cl in db2.TVItemLanguages
+                                  from ms in db2.MWQMSites
+                                  where c.TVItemID == cl.TVItemID
+                                  && c.TVItemID == ms.MWQMSiteTVItemID
+                                  && c.TVType == (int)TVTypeEnum.MWQMSite
+                                  && c.TVPath.Contains(subsectorItem.c.TVPath + "p")
+                                  && cl.Language == (int)LanguageEnum.en
+                                  && c.IsActive == true
+                                  orderby cl.TVText ascending
+                                  select new { c, cl, ms }).ToList();
+
+                var tvFileList = (from c in db2.TVItems
+                                  from cf in db2.TVFiles
+                                  where c.TVItemID == cf.TVFileTVItemID
+                                  && c.TVType == (int)TVTypeEnum.File
+                                  && c.TVPath.Contains(subsectorItem.c.TVPath + "p")
+                                  select new { c, cf }).ToList();
+
+                int TotalPageCount = (int)Math.Ceiling((tvItemList.Count / 6.0D));
+                int PageCount = 0;
+                for (int i = 0, count = tvItemList.Count; i < count; i += 6)
+                {
+                    PageCount++;
+
+                    sb.AppendLine($"<h3>{ tvText }&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;({ PageCount }/{ TotalPageCount })</h3>");
+                    sb.AppendLine(@"<table style=""width: 95%; height: 95%;"">");
+                    sb.AppendLine(@"<tr style=""height: 32%"">");
+
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if ((i + j) < tvItemList.Count)
+                        {
+                            var tvFileItem = (from c in tvFileList
+                                              where c.c.TVPath.Contains(tvItemList[i + j].c.TVPath + "p")
+                                              && c.cf.ServerFileName.Contains("monitoring.jpg")
+                                              select c).FirstOrDefault();
+
+                            if (tvFileItem == null)
+                            {
+                                sb.AppendLine(@"<td style=""width: 45%;"">");
+                                sb.AppendLine("<br><br>");
+                                sb.AppendLine($@"<span style=""font-size: 10px"">{ tvItemList[i + j].cl.TVText } No Image</span>");
+                                sb.AppendLine("<br><br>");
+                                sb.AppendLine($@"<span style=""font-size: 10px"">with name ending with monitoring.jpg</span>");
+                                sb.AppendLine("</td>");
+                            }
+                            else
+                            {
+                                sb.AppendLine(@"<td style=""width: 45%;"">");
+                                sb.AppendLine($@"|||Image|FileName,{ tvFileItem.cf.ServerFilePath }{ tvFileItem.cf.ServerFileName }|width,224|height,190|||");
+                                sb.AppendLine("<br>");
+                                if (tvItemList[i + j].ms.MWQMSiteDescription.Length > 50)
+                                {
+                                    sb.AppendLine($@"<span style=""font-size: 10px"">{ tvItemList[i + j].cl.TVText } --- { tvItemList[i + j].ms.MWQMSiteDescription.Substring(0, 50) } ...</span>");
+                                }
+                                else
+                                {
+                                    sb.AppendLine($@"<span style=""font-size: 10px"">{ tvItemList[i + j].cl.TVText } --- { tvItemList[i + j].ms.MWQMSiteDescription }</span>");
+                                }
+                                sb.AppendLine("<br>");
+                                sb.AppendLine("</td>");
+                            }
+                        }
+                        else
+                        {
+                            sb.AppendLine(@"<td style=""width: 45%;"">");
+                            sb.AppendLine("<br><br><br>");
+                            sb.AppendLine($@"&nbsp;");
+                            sb.AppendLine("</td>");
+                        }
+                        if (j % 2 != 0)
+                        {
+                            sb.AppendLine("</tr>");
+                            if (j != 5)
+                            {
+                                sb.AppendLine(@"<tr style=""height: 32%"">");
+                            }
+                        }
+                        if (j == 5)
+                        {
+                            sb.AppendLine("</tr>");
+                            sb.AppendLine("</table>");
+
+                        }
+                    }
+                }
+
+                sb.AppendLine("</body>");
+                sb.AppendLine("</html>");
+
+                FileInfo fi = new FileInfo($@"C:\CSSP\{ locator } SitePhotoAlbum.html");
 
                 StreamWriter sw = fi.CreateText();
                 sw.WriteLine(sb.ToString());
